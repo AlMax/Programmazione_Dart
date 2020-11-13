2944e1b605de0d0b7d6b3147b86985c746d9ff91
Ali Haider Maqsood <alihaider.maqsood@gmail.com>
2020-11-14 00:29:34 +0100

    prove

---
+++ test.dart
@@ -0,0 +1,12 @@
+void main() {
+  print('Hello World');
+
+  /*const array = [1, 3, 5, 7, 9];
+  int somma = 0;
+  for( int num in array ){
+    somma += num;
+  }
+  print('Somma: $somma'); */
+
+  
+}
