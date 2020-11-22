f16889ce21de682419378ae691a902319b7f5bd2
Ali Haider Maqsood <alihaider.maqsood@gmail.com>
2020-11-22 18:03:00 +0100

    test

--- test.dart
+++ test.dart
@@ -7,5 +7,48 @@ void main() {
     somma += num;
   }
   print('Somma: $somma'); */
+<<<<<<< Updated upstream
   
 }
+=======
+
+  /*const multi = 10;
+  const list = [1, 2, 3];
+  final result = list.map((x) {
+    return x * multi;
+  });
+
+  print(result); */
+
+  /*const list = [1, 2, 3, 4, 5];
+
+  Tipo firstWhere<Tipo>(List<Tipo> items, bool Function(Tipo) f,
+      {Tipo Function() orElse}) {
+    for (var item in items) {
+      if (f(item)) {
+        return item;
+      }
+    }
+    return orElse();
+  }
+
+  final int first = firstWhere(list, (x) => x == 6, orElse: () => -1);
+
+  print(first); */
+
+  /*const email = [
+    'ciao@gmail.com',
+    'ciao@ciao.com',
+    'cia@prova.com',
+    'aiuto@gmail.com'
+  ];
+
+  const dominiConosciuti = ['gmail.com'];
+
+  final mailErrate = email
+      .map((mail) => mail.split('@').last)
+      .where((dominio) => !dominiConosciuti.contains(dominio));
+
+  print(mailErrate); */
+}
+>>>>>>> Stashed changes
